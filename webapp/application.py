d3b505afc36d3eb75dd5d161aa13f8257973acd5
Andrew <andrewzhao150@gmail.com>
2022-12-01 22:42:33 -0500

    Got sentencer fixed

--- webapp/application.py
+++ webapp/application.py
@@ -7,15 +7,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from webapp.database import db_session
 from webapp.database import User
 
-bp = Blueprint('group1', __name__, url_prefix='/')
-
-@bp.route("login", methods=["GET", "POST"])
-def login():
-    '''Renders the login page.'''
-    if request.method == "POST":
-        return render_template("home.html")
-    else:
-        return render_template("login.html")
+bp = Blueprint('app', __name__, url_prefix='/', static_folder='static')
 
 @bp.route("sentencer", methods=["GET", "POST"])
 def sentencer():
